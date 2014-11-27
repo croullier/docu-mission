b1cad512a475f5606f908227bb237269311e9ad6
cbeaugrand@sts-sio-caen.info <cbeaugrand@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-11-27 09:35:49 +0000

    Ajout d'utilisateur grâce au formulaire

--- dokuMission/application/controllers/Utilisateurs.php
+++ dokuMission/application/controllers/Utilisateurs.php
@@ -13,22 +13,30 @@ class Utilisateurs extends \CI_Controller {
 		$this->load->library('form_validation');
 	
 		$this->form_validation->set_rules('login', 'Login', 'trim|required|min_length[5]|max_length[12]|xss_clean');
+		$this->form_validation->set_rules('groupe', 'Groupe', 'trim|required|min_length[1]|max_length[12]|xss_clean');
+		$this->form_validation->set_rules('monde', 'Monde', 'trim|required|min_length[1]|max_length[12]|xss_clean');
 		if ($this->form_validation->run() == FALSE)
 		{
 			$this->load->view('v_utilisateur_add');
 		}
 		else
 		{
-			$this->submit_add($_POST["login"]);
+			$this->submit_add($_POST["login"],$_POST["groupe"],$_POST["monde"]);
 		}
 	}
 	
-	public function submit_add($login){
+	public function submit_add($login,$groupe,$monde){
 		$user = new Utilisateur();
 		$user->setLogin($login);
+		$query = $this->doctrine->em->createQuery("SELECT g FROM Groupe g WHERE g.id=".$groupe);
+		$groupe = $query->getSingleResult();
+		$user->setGroupe($groupe);
+		$query = $this->doctrine->em->createQuery("SELECT m FROM Monde m WHERE m.id=".$monde);
+		$monde = $query->getSingleResult();
+		$user->setIdmonde($monde);
 		$this->doctrine->em->persist($user);
 		$this->doctrine->em->flush();
-		$this->load->view('v_success_add',array('user'=>$user));
+		$this->load->view('v_success_add',array('user'=>$user,'groupe'=>$groupe,'monde'=>$monde));
 	}
 	
 }
--- dokuMission/application/views/v_groupes.php
+++ dokuMission/application/views/v_groupes.php
@@ -1,5 +1,5 @@
 <?php
-
+var_dump($groupes);
 foreach ($groupes as $group){
 	echo $group->getlibelle()." (";
 	 foreach ($group->getUtilisateurs() as $g){
