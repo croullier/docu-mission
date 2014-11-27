c05199440f80e41772790654a28c9afe03b98a0a
cbeaugrand@sts-sio-caen.info <cbeaugrand@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-11-27 09:35:49 +0000

    Ajout d'utilisateur grâce au formulaire

--- trunk/dokuMission/application/views/v_utilisateur_add.php
+++ trunk/dokuMission/application/views/v_utilisateur_add.php
@@ -9,7 +9,11 @@
 <?php echo form_open('utilisateurs/add/'); ?>
  
 <h5>Nom d'utilisateur</h5>
-<input type="text" name="login" value="<?php echo set_value('login'); ?>" size="50" />
+<input type="text" name="login" value="<?php echo set_value('login'); ?>" size="50" /><br>
+<h5>groupe</h5>
+<input type="text" name="groupe" value="<?php echo set_value('goupe'); ?>" size="50" /><br>
+<h5>monde</h5>
+<input type="text" name="monde" value="<?php echo set_value('monde'); ?>" size="50" />
  
 <div><input type="submit" value="Ajouter utilisateur" /></div>
  
