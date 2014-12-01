9fcab00c6b79ceae15fd5a65219e686f602fe0dd
cbeaugrand@sts-sio-caen.info <cbeaugrand@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-12-01 10:25:48 +0000

    css

--- trunk/dokuMission/application/controllers/MyDocs.php
+++ trunk/dokuMission/application/controllers/MyDocs.php
@@ -2,7 +2,7 @@
 class MyDocs extends \CI_Controller {
 	
 	public function index(){
-		$query = $this->doctrine->em->createQuery("SELECT d FROM Document d");
+		$query = $this->doctrine->em->createQuery("SELECT d FROM Document d WHERE");
 		$document = $query->getResult();
 		$this->load->view('v_myDocs',array('documents'=>$document));
 	}
--- trunk/dokuMission/application/helpers/assets_helper.php
+++ trunk/dokuMission/application/helpers/assets_helper.php
@@ -4,15 +4,15 @@ if (!defined('ASSETS_HELPER')){
     define('ASSETS_HELPER',true);
     function css_url($nom)
     {
-        return base_url() . 'assets/css/' . $nom . '.css';
+        return 'assets/css/' . $nom . '.css';
     }
     function js_url($nom)
     {
-        return base_url() . 'assets/javacript/' . $nom . '.js';
+        return 'assets/javacript/' . $nom . '.js';
     }
     function img_url($nom)
     {
-        return base_url() . 'assets/images/' . $nom;
+        return 'assets/images/' . $nom;
     }
     function img($nom, $alt = '')
     {
