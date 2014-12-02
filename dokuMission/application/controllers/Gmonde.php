2c0a9036a00b391425671ef6e36117ef419e7884
pmarroig@sts-sio-caen.info <pmarroig@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-12-02 13:46:10 +0000

    gestion monde

--- dokuMission/application/controllers/Gmonde.php
+++ dokuMission/application/controllers/Gmonde.php
@@ -1,8 +1,41 @@
 <?php
 class Gmonde extends \CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->library('jsUtils');
+	}
+	
 	public function index(){
 		$query = $this->doctrine->em->createQuery("SELECT m FROM Monde m");
 		$monde = $query->getResult();
-		$this->load->view('v_monde',array('mondes'=>$monde));
+		$this->load->view('v_monde',array('mondes'=>$monde));		
+		$this->jsutils->getAndBindTo("#update","click","Gmonde/monde_modif","#modifier");
+		$this->jsutils->external();
+		$this->jsutils->compile();
+	}
+	
+	public function add(){	
+		$this->load->library('form_validation');	
+		$this->form_validation->set_rules('monde', 'Monde', 'trim|required|min_length[1]|max_length[12]|xss_clean');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('v_monde');
+		}
+		else
+		{
+			$this->monde_add($_POST["monde"]);
+		}
+	}
+	
+	public function monde_add($monde){
+		$mondes = new Monde();
+		$mondes->setLibelle($monde);
+		$this->doctrine->em->persist($mondes);
+		$this->doctrine->em->flush();
+	}
+	
+	public function monde_modif(){
+		$this->jsUtils->doSomeThingOn("#modifier","show");
+		echo"lol";
 	}
 }
