cd5ba3c3dcb15cbbd509cace8ba1968617232c45
cbeaugrand@sts-sio-caen.info <cbeaugrand@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-12-04 16:10:06 +0000

    Mise en place du full ajax avec formulaire. Mise en place des fonctionnalité (insèrer, modifier et supprimer).

--- trunk/dokuMission/application/views/v_monde.php
+++ trunk/dokuMission/application/views/v_monde.php
@@ -3,7 +3,7 @@
  <?php echo $library_src;?>
 <?php echo $script_foot;?>
 <?php 
-echo validation_errors();
+/*echo validation_errors();
 echo form_open('Gmonde/add/');
 
 foreach ($mondes as $monde){
@@ -13,7 +13,7 @@ foreach ($mondes as $monde){
 ?>
 
 <br>
-Ajouter: <input type='text' name='monde'><input type='submit' value='Insérer'>
+
 
 <?php
 echo form_close();
@@ -24,7 +24,25 @@ echo form_close();
 		Modifier: <input type='text' name='update_monde'><input type='submit' value='Insérer'>
 
 	</div>
-	<?php echo form_close(); ?>
+	<?php echo form_close();*/ 
+
+foreach ($mondes as $monde){
+	echo("<table border=1><tr><td>".$monde->getLibelle()."</td><td><a href='#' class='update' id='".$monde->getId()."'>Modif</a></td><td><a href='#' class='delete' id='".$monde->getId()."'>Suppr</a></td></tr></table>");
+}
+
+?>
+	<form id="frmAddMonde" name="frmAddMonde">
+		<label for="libelle">Ajouter:</label> <input type="text" name="libelle" id="libelle">
+		<input type="button" value="Ajouter" id="btAdd">
+	</form>
+	
+	<div id="modifier" style="display:none;">
+		<form id="frmUpdateMonde" name="frmUpdateMonde">
+			<label for="libelle">Modifier:</label> <input type="text" name="libelle" id="libelle">
+			<input type="button" value="Modifier" id="btUpdate">
+		</form>
+	</div>
+	
 <div id="message">
 </div>
 
