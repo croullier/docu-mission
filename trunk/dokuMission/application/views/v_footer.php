ec97b3e7778ee84f031649683362178d106b89a8
CRoullier@sts-sio-caen.info <CRoullier@sts-sio-caen.info@d07064a2-b0fe-b28c-1dc3-5d9249072a07>
2014-12-18 11:11:51 +0000

    listages

--- trunk/dokuMission/application/views/v_footer.php
+++ trunk/dokuMission/application/views/v_footer.php
@@ -1,10 +1,15 @@
-</body>
-<footer>
 
+<footer>
 	<hr>
+<<<<<<< .mine
+	<center>
+		Copyright &copy;  | Tous droits reserves | 2014
+	</center>
+=======
 		<center>
 			Copyright &copy;  | Tous droits réservés | 2014
 		</center>
 
+>>>>>>> .r36
 </footer>
 </html>
